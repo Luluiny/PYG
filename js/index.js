7f034363d82c5b3ca1ae7c73f8fc6a40a31a361b
Luluiny <Luluiny@163.com>
2018-08-10 22:38:08 +0800

    完成首页

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ $(function () {
             //console.log(res);
             var html=template("tempslider",{data:res.data});
             $(".mui-slider").html(html);
-            //初始化轮播图
+            //初始化轮播图  写在这里原因是一般要等标签渲染后然后初始化
             var gallery = mui('.mui-slider');
             gallery.slider({
               interval: 1000//自动轮播周期，若为0则不自动播放，默认为0；
