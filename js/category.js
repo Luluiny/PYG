7eee0bba26cfa07597b18a06e50c238c326d13c8
Luluiny <Luluiny@163.com>
2018-08-11 21:39:49 +0800

    完成分类页面

--- js/category.js
+++ js/category.js
@@ -1,14 +1,64 @@
 $(function(){
+    var Datas;
+    var leftScroll;
     init();
+   
     function init(){
         setHtml();
-        getData();
+        eventList();
+        getCategories();
     }
     
-    function getData(){
-        //只有元素的第一个子元素才可以滚动
-        var leftScroll = new IScroll(".left");
+    function eventList(){
+        //用委托的方式对左侧的注册点击事件
+        $(".left").on("tap","li",function(){
+            //点击到li标签中的第几个 就显示返回值中的第几个
+            //使用自定义标签记录 直接获取li标签的索引
+            //获取自定义属性的值
+                // <div id="text" value="123"  data-name="黑哒哒的盟友"><div>
+                // $("#text").data("name");
+            var index=$(this).data("index");
+            $(this).addClass("active").siblings().removeClass("active");
+            //该项向上滚动置顶
+            leftScroll.scrollToElement(this);
+            renderRight(index);
+        })
+        
     }
+
+
+    function getCategories(){
+        $.get("categories",function(res){
+            //console.log(res);
+            var html=template("tempcateleft",{data:res.data});
+            $(".cateleft").html(html);
+            //只有容器元素的第一个子元素可以滚动 其他被忽略
+            leftScroll = new IScroll(".left");
+            Datas=res.data;
+            renderRight(0);
+        })
+
+    }
+
+
+    //根据索引渲染右边的数据
+    function renderRight(index){
+        var arr=Datas[index].children;
+        //console.log(arr);
+        var html2=template("tempcateright",{data:arr});
+        $(".right").html(html2);
+        //初始化右边的滚动条 标签都加载完了 图片不一定加载完了 要在所有的标签图片加载好了才开始初始化
+        // //声明一个变量来保存图片的张数
+        var nums=$(".right img").length;
+        $(".right img").on("load",function(){
+            console.log(123);
+            nums--;
+            if(nums==0){
+                new IScroll(".right");
+            }
+        })
+    }
+
     function  setHtml(){
         //根据屏幕试着字体的大小
         //设计的宽度/基础值=要适配的屏幕的宽度/fz
@@ -24,11 +74,5 @@ $(function(){
         setHtml();
     }
 
-    $.get("categories",function(res){
-        console.log(res);
-        var html=template("tempcateleft",{data:res.data});
-        $(".cateleft").html(html);
-        var htmll=template("tempcateright",{data:res.data});
-        
-    })
+    
 })
--- js/common.js
+++ js/common.js
@@ -1,9 +1,10 @@
 $(function(){
-    var BaseUrl="http://api.pyg.ak48.xyz/api/public/v1/";
+    var BaseUrl="http://api.pyg.ak48.xyz/";
+    template.defaults.imports.iconUrl = BaseUrl;
     //修改接口的使用方式 拦截器 在每一次发送请求之前 对请求做一些处理
     //一般的地址修改做出改变是只改变前面的接口之类的 后面的分类的一般是
     //很少改变 所以把前面的容易做出改变的拿出来 单独放在一个变量里 这样就会比较容易管理 后面的地址也是可以改变的 只是比较少
     $.ajaxSettings.beforeSend=function(xhr,obj){
-        obj.url=BaseUrl+obj.url;
+        obj.url=BaseUrl+"api/public/v1/"+obj.url;
     }
 })
